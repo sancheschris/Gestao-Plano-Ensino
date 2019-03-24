d7bf7a6fe4c115b06f1cb461c4f86de34153023d
Christian Sanches dos Santos <sancheschris@gmail.com>
2019-03-24 20:37:26 -0300

    Add security for two controllers

--- API/Controllers/UsuariosController.cs
+++ API/Controllers/UsuariosController.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class UsuariosController : ControllerBase
     {
         private readonly APIContext _context;
